2b19abb451511c71234b54fdf1b3b5a82ebdef92
chrisDvlp <120775948+chrisDvlp@users.noreply.github.com>
2022-12-25 18:16:26 +0100

    menu screen
    added title screen, start and quit buttons

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    } 
+}
